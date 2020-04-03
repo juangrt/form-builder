1459501c0291c102eecbd93425b65158674e1e2c
Nuno Namorado <n.namorado@runtime-revolution.com>
2020-04-03 11:53:25 +0100

    Updates embeded example

--- src/components/embeded.js
+++ src/components/embeded.js
@@ -4,7 +4,7 @@ import './embeded.css'
 export default function() {
   return (
     <div className="embeded">
-      <iframe src="https://nunonamorado.formstack.com/forms/supplemental_form" title="supplemental form" width="600" height="800"></iframe>
+      <iframe src="https://nunonamorado.formstack.com/forms/income_driven_repayment_plan_request_form_copy" title="Income Driven Repayment Plan Request Form" width="800" height="800"></iframe>
     </div>
   );
 }
